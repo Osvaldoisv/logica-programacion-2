0dec20bf910f7c51c1a4a625e18ca5d5740a3dbe
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-01-11 16:56:13 -0300

    Intento1

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function obtenerInformacion() {
+    let temperatura = parseFloat(document.getElementById("Entrada").value);
+
+    kel = temperatura + 273.15;
+    fah = (temperatura * 9/5) + 32;
+
+    let g_celsius = document.getElementById("celsius");
+    let g_kelvin = document.getElementById("kelvin");
+    let g_fahrenheit = document.getElementById("fahrenheit");
+
+    g_celsius.innerHTML = temperatura  + " °C";
+    g_kelvin.innerHTML = kel + " °K";
+    g_fahrenheit.innerHTML = fah + " °F";
+}
+
+
